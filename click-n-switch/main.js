e7bced72e1559004fdb78457e67c895183d70e8d
manuelbarzi <manuelbarzi@gmail.com>
2021-10-04 19:18:33 +0200

    remove top menu click adaptive demo (move it to css-fx-demo)

--- click-n-switch/main.js
+++ click-n-switch/main.js
@@ -21,10 +21,3 @@ smiley.onclick = function() {
         smiley.innerText = '😢'
     }
 }
-
-var menuButton = document.querySelector('.menu__button')
-var menuList = document.querySelector('.menu__list')
-
-menuButton.onclick = function() {
-    menuList.classList.toggle('menu__list--on')
-}
